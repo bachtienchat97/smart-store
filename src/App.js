f76062cc92257df2c678a33c07feb4b4fd1a66fb
Bach Tien Chat <bachtienchat@gmail.com>
2023-01-12 00:26:13 +0700

    user register feature done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import LayoutDefault from "./layout/LayoutDefault";
 import Home from './views/pages/Home';
 import Product from "./views/pages/Product";
 import Login from "./views/pages/Login";
+import NotFound404 from "views/pages/NotFound404";
+import { LoginForm, RegisterForm } from "components/user";
 
 import { 
   createBrowserRouter,
   createRoutesFromElements,
-  Navigate,
   Route,
   RouterProvider
 } from "react-router-dom";
-import NotFound404 from "views/pages/NotFound404";
 
 
 const router = createBrowserRouter(
@@ -18,7 +18,11 @@ const router = createBrowserRouter(
       <Route path="/" element={<LayoutDefault/>}>
         <Route path="" element={<Home/>} />
         <Route path="products" element={<Product/>} />
-        <Route path="login" element={<Login/>} />
+
+        <Route path="user" element={<Login/>}>
+          <Route path="login" element={<LoginForm/>} />
+          <Route path="register" element={<RegisterForm/>} />
+        </Route>
         <Route path="*" element={<NotFound404/>} />
         {/* <Route path="*" element={<Navigate to="" replace />} /> */}
       </Route>
